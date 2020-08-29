0650c7c097f572486612298558bcdeef1bc05773
Thomas Rowe <thomasrowe101@gmail.com>
2020-08-28 17:26:11 -0700

    Cleanups and dependency fix.

--- client/src/main/kotlin/org/example/client/service/D6WebClient.kt
+++ client/src/main/kotlin/org/example/client/service/D6WebClient.kt
@@ -4,8 +4,6 @@ import org.example.client.exception.D6RemoteClientException.WebCallException
 import org.example.client.model.SynchronousClientInput.HttpClientInput
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec
-import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec
 
 // TODO: Add documentation.
 class D6WebClient(
@@ -16,32 +14,17 @@ class D6WebClient(
         input: HttpClientInput<InputType, ReturnType>,
         returnType: Class<out ReturnType>
     ): ReturnType {
-        val request = if(input.payload != null)
-            withBody(input)
-        else
-            withoutBody(input)
-
-        return request
+        return webClient
+            .method(input.method)
+            .uri(input.uri)
+            .apply {
+                input.payload?.let {
+                    body(BodyInserters.fromValue(input.payload))
+                }
+            }
             .retrieve()
             .bodyToMono(returnType)
             .block()
             ?: throw WebCallException(input.uri)
-    }
-
-    private fun <InputType, ReturnType> withBody(
-        input: HttpClientInput<InputType, ReturnType>
-    ): RequestHeadersSpec<*> {
-        return webClient
-            .method(input.method)
-            .uri(input.uri)
-            .body(BodyInserters.fromValue(input.payload!!))
-    }
-
-    private fun <InputType, ReturnType> withoutBody(
-        input: HttpClientInput<InputType, ReturnType>
-    ): RequestBodySpec {
-        return webClient
-            .method(input.method)
-            .uri(input.uri)
     }
 }
